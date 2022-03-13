magic-akari/esbuild

commit 4fa3d7a56145309087a12c2f5f6ae7b88e315082

fix #610: minify now drops overwritten functions

internal/js_ast/js_ast.go
@@ -1561,6 +1561,14 @@ const (
 	//   Foo.#foo = Foo;
 	//
 	PrivateSymbolMustBeLowered
+
+	// This is used to remove the all but the last function re-declaration if a
+	// function is re-declared multiple times like this:
+	//
+	//   function foo() { console.log(1) }
+	//   function foo() { console.log(2) }
+	//
+	RemoveOverwrittenFunctionDeclaration
 )
 
 func (flags SymbolFlags) Has(flag SymbolFlags) bool {

internal/js_parser/js_parser.go
@@ -1426,7 +1426,7 @@ func (p *parser) canMergeSymbols(scope *js_ast.Scope, existing js_ast.SymbolKind
 	if new.IsHoistedOrFunction() && existing.IsHoistedOrFunction() &&
 		(scope.Kind == js_ast.ScopeEntry || scope.Kind == js_ast.ScopeFunctionBody ||
 			(new.IsHoisted() && existing.IsHoisted())) {
-		return mergeKeepExisting
+		return mergeReplaceWithNew
 	}
 
 	// "get #foo() {} set #foo() {}"
@@ -1490,6 +1490,11 @@ func (p *parser) declareSymbol(kind js_ast.SymbolKind, loc logger.Loc, name stri
 
 		case mergeReplaceWithNew:
 			symbol.Link = ref
+
+			// If these are both functions, remove the overwritten declaration
+			if p.options.mangleSyntax && kind.IsFunction() && symbol.Kind.IsFunction() {
+				symbol.Flags |= js_ast.RemoveOverwrittenFunctionDeclaration
+			}
 
 		case mergeBecomePrivateGetSetPair:
 			ref = existing.Ref
@@ -9484,6 +9489,11 @@ func (p *parser) visitAndAppendStmt(stmts []js_ast.Stmt, stmt js_ast.Stmt) []js_
 
 	case *js_ast.SFunction:
 		p.visitFn(&s.Fn, s.Fn.OpenParenLoc)
+
+		// Strip this function declaration if it was overwritten
+		if p.symbols[s.Fn.Name.Ref.InnerIndex].Flags.Has(js_ast.RemoveOverwrittenFunctionDeclaration) && !s.IsExport {
+			return stmts
+		}
 
 		// Handle exporting this function from a namespace
 		if s.IsExport && p.enclosingNamespaceArgRef != nil {

internal/js_parser/js_parser_test.go
@@ -1342,6 +1342,21 @@ func TestFunction(t *testing.T) {
 	expectPrintedMangle(t, "function* foo() { return undefined }", "function* foo() {\n}\n")
 	expectPrintedMangle(t, "async function foo() { return undefined }", "async function foo() {\n}\n")
 	expectPrintedMangle(t, "async function* foo() { return undefined }", "async function* foo() {\n  return void 0;\n}\n")
+
+	// Strip overwritten function declarations
+	expectPrintedMangle(t, "function f() { x() } function f() { y() }", "function f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function f() { x() } function *f() { y() }", "function* f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function *f() { x() } function f() { y() }", "function f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function *f() { x() } function *f() { y() }", "function* f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function f() { x() } async function f() { y() }", "async function f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "async function f() { x() } function f() { y() }", "function f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "async function f() { x() } async function f() { y() }", "async function f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "var f; function f() {}", "var f;\nfunction f() {\n}\n")
+	expectPrintedMangle(t, "function f() {} var f", "function f() {\n}\nvar f;\n")
+	expectPrintedMangle(t, "var f; function f() { x() } function f() { y() }", "var f;\nfunction f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function f() { x() } function f() { y() } var f", "function f() {\n  y();\n}\nvar f;\n")
+	expectPrintedMangle(t, "function f() { x() } var f; function f() { y() }", "function f() {\n  x();\n}\nvar f;\nfunction f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "export function f() { x() } function f() { y() }", "export function f() {\n  x();\n}\nfunction f() {\n  y();\n}\n")
 }
 
 func TestClass(t *testing.T) {

commit 47802b7300014e2220f8a816a31356418aeab3ca

avoid var declaration overwritten
- fix #2080 regression introduced in 4fa3d7a

internal/js_ast/js_ast.go
@@ -1482,10 +1482,11 @@ type Scope struct {
 	// This will be non-nil if this is a TypeScript "namespace" or "enum"
 	TSNamespace *TSNamespaceScope
 
-	Parent    *Scope
-	Children  []*Scope
-	Members   map[string]ScopeMember
-	Generated []Ref
+	Parent     *Scope
+	Children   []*Scope
+	Members    map[string]ScopeMember
+	HoistFnRef map[string]*Ref
+	Generated  []Ref
 
 	// The location of the "use strict" directive for ExplicitStrictMode
 	UseStrictLoc logger.Loc

internal/js_parser/js_parser.go
@@ -910,10 +910,11 @@ func (p *parser) selectLocalKind(kind js_ast.LocalKind) js_ast.LocalKind {
 func (p *parser) pushScopeForParsePass(kind js_ast.ScopeKind, loc logger.Loc) int {
 	parent := p.currentScope
 	scope := &js_ast.Scope{
-		Kind:    kind,
-		Parent:  parent,
-		Members: make(map[string]js_ast.ScopeMember),
-		Label:   js_ast.LocRef{Ref: js_ast.InvalidRef},
+		Kind:       kind,
+		Parent:     parent,
+		Members:    make(map[string]js_ast.ScopeMember),
+		HoistFnRef: make(map[string]*js_ast.Ref),
+		Label:      js_ast.LocRef{Ref: js_ast.InvalidRef},
 	}
 	if parent != nil {
 		parent.Children = append(parent.Children, scope)
@@ -1107,6 +1108,7 @@ const (
 	mergeForbidden = iota
 	mergeReplaceWithNew
 	mergeOverwriteWithNew
+	mergeHoistFunction
 	mergeKeepExisting
 	mergeBecomePrivateGetSetPair
 	mergeBecomePrivateStaticGetSetPair
@@ -1147,14 +1149,12 @@ func (p *parser) canMergeSymbols(scope *js_ast.Scope, existing js_ast.SymbolKind
 		}
 	}
 
-	// "var foo; var foo;"
-	// "var foo; function foo() {}"
-	// "function foo() {} var foo;"
+	// only top level function can be overwritten
 	// "function *foo() {} function *foo() {}" but not "{ function *foo() {} function *foo() {} }"
 	if new.IsHoistedOrFunction() && existing.IsHoistedOrFunction() &&
 		(scope.Kind == js_ast.ScopeEntry || scope.Kind == js_ast.ScopeFunctionBody ||
 			(new.IsHoisted() && existing.IsHoisted())) {
-		return mergeReplaceWithNew
+		return mergeHoistFunction
 	}
 
 	// "get #foo() {} set #foo() {}"
@@ -1219,9 +1219,25 @@ func (p *parser) declareSymbol(kind js_ast.SymbolKind, loc logger.Loc, name stri
 		case mergeReplaceWithNew:
 			symbol.Link = ref
 
-			// If these are both functions, remove the overwritten declaration
-			if p.options.minifySyntax && kind.IsFunction() && symbol.Kind.IsFunction() {
-				symbol.Flags |= js_ast.RemoveOverwrittenFunctionDeclaration
+		case mergeHoistFunction:
+			if symbol.Kind.IsFunction() {
+				p.currentScope.HoistFnRef[name] = &existing.Ref
+			}
+
+			if kind.IsFunction() {
+				if p.options.minifySyntax {
+					fnRef := p.currentScope.HoistFnRef[name]
+					if fnRef != nil {
+						fnSymbol := &p.symbols[fnRef.InnerIndex]
+						fnSymbol.Flags |= js_ast.RemoveOverwrittenFunctionDeclaration
+					}
+				}
+
+				p.currentScope.HoistFnRef[name] = &ref
+			}
+
+			if !symbol.Kind.IsFunction() && !kind.IsFunction() {
+				ref = existing.Ref
 			}
 
 		case mergeBecomePrivateGetSetPair:

internal/js_parser/js_parser_test.go
@@ -1357,8 +1357,10 @@ func TestFunction(t *testing.T) {
 	expectPrintedMangle(t, "function f() {} var f", "function f() {\n}\nvar f;\n")
 	expectPrintedMangle(t, "var f; function f() { x() } function f() { y() }", "var f;\nfunction f() {\n  y();\n}\n")
 	expectPrintedMangle(t, "function f() { x() } function f() { y() } var f", "function f() {\n  y();\n}\nvar f;\n")
-	expectPrintedMangle(t, "function f() { x() } var f; function f() { y() }", "function f() {\n  x();\n}\nvar f;\nfunction f() {\n  y();\n}\n")
+	expectPrintedMangle(t, "function f() { x() } var f; function f() { y() }", "var f;\nfunction f() {\n  y();\n}\n")
 	expectPrintedMangle(t, "export function f() { x() } function f() { y() }", "export function f() {\n  x();\n}\nfunction f() {\n  y();\n}\n")
+
+	expectPrintedMangle(t, "var x = x || {}; console.log(x); var x = x || {};", "var x = x || {};\nconsole.log(x);\nvar x = x || {};\n")
 }
 
 func TestClass(t *testing.T) {